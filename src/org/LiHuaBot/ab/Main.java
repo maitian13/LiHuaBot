43192aad453926aa47955eefb3190b0d47337330
maitian13 <maitianshouwei@163.com>
2016-01-31 21:31:42 +0800

    Main Class

---
+++ src/org/LiHuaBot/ab/Main.java
@@ -0,0 +1,10 @@
+package org.LiHuaBot.ab;
+
+/**
+ * Created by maitian13 on 2016/1/31.
+ */
+public class Main {
+    public static void main(String args[]){
+        System.out.println("hello world!");
+    }
+}
